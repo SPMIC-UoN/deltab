Corrected normalization of features and prediction of delta in alternate model

brain_delta/brain_delta.py
@@ -42,45 +42,44 @@ class BrainDelta:
         :param features: 2D array of [subjects, features]
         :param ev_proportion: Optional proportion of features to be retained in model
         :param ev_num: Optional number of features to be retained in model
-        :param include_quad: If True, include quadratic variation in age dependence
         """
-        if ev_proportion is not None and ev_num is not None:
+        if ev_proportion and ev_num:
             raise ValueError("Only one of ev_proportion and ev_num may be specified")
 
         # 1. Your vector of ages is Y (subjects 1)
-        self.y = np.squeeze(ages)
-        if self.y.ndim != 1:
+        self.ytrain = np.squeeze(ages)
+        if self.ytrain.ndim != 1:
             raise ValueError("Ages must be a 1D array")
-        num_subjects = len(self.y)
+        self.num_subjects_train = len(self.ytrain)
 
-        # 2. Your matrix of brain imaging measures is X (subjects features/ voxels)
-        self.x = np.array(features)
-        if self.x.ndim != 2:
+        # 2. Your matrix of brain imaging measures is X (subjects features/voxels)
+        self.xtrain = np.array(features)
+        if self.xtrain.ndim != 2:
             raise ValueError("Features must be a 2D array")
-        if self.x.shape[0] != num_subjects:
+        if self.xtrain.shape[0] != self.num_subjects_train:
             raise ValueError("Number of subjects does not match number of rows in feature matrix")
-                
-        # 3. Subtract the means from Y and all columns in X
-        self.y_mean = np.mean(self.y)
-        self.x_mean = np.mean(self.x, axis=0)
-        self.y_demean = self.y - self.y_mean
-        self.x_demean = self.x - self.x_mean
+        self.num_features_train = self.xtrain.shape[1]
 
-        if ev_num is not None or ev_proportion is not None:
+        # 3. Subtract the means from Y and all columns in X
+        self.y_mean = np.mean(self.ytrain)
+        self.y_demean = self._standardize_y(self.ytrain)
+
+        if ev_num or ev_proportion:
             # 4. Use SVD to replace X with its top 10–25% vertical eigenvectors
             # Note that np.linalg.svd returns eigenvalues/vectors sorted in descending
             # order as we require
-            if ev_num is not None:
+            if ev_num:
                 self.ev_num = ev_num
             else:
                 self.ev_num = max(1, int(ev_proportion * self.x_demean.shape[1]))
             self.pca = PCA(n_components=self.ev_num)
-            self.x_reduced = self.pca.fit_transform(self.x_demean)
+            self.x_reduced = self.pca.fit_transform(self.xtrain)
         else:
             # No reduction of features
-            self.ev_num = self.x_demean.shape[1]
+            self.ev_num = self.xtrain.shape[1]
             self.pca = None
-            self.x_reduced = self.x_demean
+            self.x_reduced = self.xtrain
+        self.x_norm, self.x_mean, self.x_std = self._normalize(self.x_reduced)
 
         # 5. Compute Y2, demean it and orthogonalise it with respect to Y to give Y2 o
         self.ysq = np.square(self.y_demean)
@@ -88,6 +87,7 @@ class BrainDelta:
         self.ysq_demean = self.ysq - self.ysq_mean
         self.ysq_orth_offset = np.dot(self.y_demean/np.linalg.norm(self.y_demean), self.ysq_demean)
         self.ysq_orth = self._orthogonalize(self.ysq_demean, self.y_demean)
+
         # 6. Create matrix [Y2 Y2o]
         self.y2sq = np.array([self.y_demean, self.ysq_orth]).T
 
@@ -96,25 +96,38 @@ class BrainDelta:
 
         # 7. The initial model is Y B1 = X β1 + δ1. Do:
         #    (a) Compute initial age prediction β1 = X^-1 Y giving Y_B1 = X β1 (where X^-1 is the pseudo-inverse of X). 
-        self.b1 = np.dot(np.linalg.pinv(self.x_reduced), self.y_demean)
-        self.y_b1 = np.dot(self.x_reduced, self.b1)
+        self.b1 = np.dot(np.linalg.pinv(self.x_norm), self.y_demean)
+        self.y_b1 = np.dot(self.x_norm, self.b1)
 
         #    (b) Compute initial brain age delta δ1 = Y_B1 Y. 
-        self.d1 = self.y_b1 - self.y_demean
+        d1 = self.y_b1 - self.y_demean
 
         # 8. The corrected model is δ1 = Y2 β2 + δ2q. Do: 
         #    (a) Compute corrected model fit β2 = Y2^-1 δ1 (correcting for bias in the initial fit and quadratic brain aging).
-        self.b2 = np.dot(np.linalg.pinv(self.y2), self.d1)
-        self.b2sq = np.dot(np.linalg.pinv(self.y2sq), self.d1)
-
-        #    (b) Compute final brain age delta δ2q = δ1 - Y2 β2
-        self.d2 = self.d1 - np.dot(self.y2sq, self.b2sq)
-        self.y_b2 = self.d2 + self.y_demean
-        self._trained = True
+        self.b2 = np.dot(np.linalg.pinv(self.y2), d1)
+        self.b2sq = np.dot(np.linalg.pinv(self.y2sq), d1)
 
         # Alternative model
-        self.gamma = np.dot(np.linalg.pinv(self.y2), self.x_reduced)
-        self.gammasq = np.dot(np.linalg.pinv(self.y2sq), self.x_reduced)
+        self.gamma = np.dot(np.linalg.pinv(self.y2), self.x_norm)
+        self.gammasq = np.dot(np.linalg.pinv(self.y2sq), self.x_norm)
+
+        self._trained = True
+
+    def _demean(self, x, axis=0):
+        return x - np.nanmean(x, axis=axis)
+
+    def _normalize(self, x, mean=None, std=None, axis=0):
+        if mean is None:
+            mean=np.mean(x, axis=axis)
+        if std is None:
+            std = np.nanstd(x, axis=axis)
+        return (x-mean) / std, mean, std
+
+    def _standardize_y(self, y):
+        return y - self.y_mean
+
+    def _standardize_x(self, x):
+        return self._normalize(x, mean=self.x_mean, std=self.x_std)[0]
 
     def _orthogonalize(self, a, b):
         return a - np.dot(b/np.linalg.norm(b), a)
@@ -140,16 +153,15 @@ class BrainDelta:
             raise ValueError("Features must be 1D or 2D array")
         if features.shape[0] != age.shape[0]:
             raise ValueError("Number of subjects must match in features and age arrays")
-        if features.shape[1] != self.x.shape[1]:
+        if features.shape[1] != self.num_features_train:
             raise ValueError("Number of features must match training features")
 
-        age_demean = age - self.y_mean
-        features_demean = features - self.x_mean
+        age_demean = self._standardize_y(age)
         if self.pca is not None:
-            features_reduced = self.pca.transform(features_demean)
-        else:
-            features_reduced = features_demean
+            features = self.pca.transform(features)
+        features_norm = self._standardize_x(features)
 
+        # Generate the quadratic age dependence if we need it
         if model in (Model.UNBIASED_QUADRATIC, Model.ALTERNATE_QUADRATIC):
             agesq = np.square(age_demean)
             agesq_demean = agesq - np.mean(agesq)
@@ -158,27 +170,31 @@ class BrainDelta:
         else:
             y2 = age_demean[:, np.newaxis]
 
+        # Generate the prediction
         if model == Model.ALTERNATE_QUADRATIC:
             #print("dot2: ", y2.shape, self.gammasq.shape)
-            d = features_reduced - np.dot(y2, self.gammasq)
-            age_predict = np.squeeze(np.dot(d, np.linalg.pinv(self.gammasq[np.newaxis, 0, :])) + self.y_mean)
-            #print(age_predict.shape)
+            d = features_norm - np.dot(y2, self.gammasq)
+            delta_predict = np.squeeze(np.dot(d, np.linalg.pinv(self.gammasq[np.newaxis, 0, :])) )
+            #print(delta_predict.shape)
         elif model == Model.ALTERNATE:
             #print("dot: ", age_demean.shape, self.gamma.shape)
-            d = features_reduced - np.dot(age_demean[..., np.newaxis], self.gamma)
-            age_predict = np.squeeze(np.dot(d, np.linalg.pinv(self.gamma)) + self.y_mean)
-            #print(age_predict.shape)
+            d = features_norm - np.dot(age_demean[..., np.newaxis], self.gamma)
+            #print("d", d.shape, np.mean(d), np.std(d))
+            delta_predict = np.squeeze(np.dot(d, np.linalg.pinv(self.gamma)))
+            #print(delta_predict.shape)
         else:
-            age_predict = np.dot(features_reduced, self.b1) + self.y_mean
+            # Compute initial brain age delta δ1 = Y_B1 Y. 
+            delta_predict = np.dot(features_norm, self.b1) - age_demean
+            #  For unbiased models, compute final brain age delta δ2q = δ1 - Y2 β2
             if model == Model.UNBIASED_QUADRATIC:
-                age_predict -= np.dot(y2, self.b2sq)
+                delta_predict -= np.dot(y2, self.b2sq)
             elif model == Model.UNBIASED:
-                age_predict -= np.dot(y2, self.b2)
+                delta_predict -= np.dot(y2, self.b2)
 
         if return_delta:
-            return age_predict - age
+            return delta_predict
         else:
-            return age_predict
+            return delta_predict + age
 
 if __name__ == "__main__":
     # How many subjects for each true age