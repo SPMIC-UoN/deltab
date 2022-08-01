5d37e25723d5be8cb421fb105a086a041d0cb4b9
Martin Craig <martin.craig@nottingham.ac.uk>
2022-08-01 21:23:11 +0100

    Clean up and improve commenting and naming

--- examples/smith_results_table1.py
+++ examples/smith_results_table1.py
@@ -3,14 +3,30 @@ Table 1 from Smith et al
 """
 import numpy as np
 from matplotlib import pyplot as plt
-import sys
 import pandas as pd
 
 from brain_delta.brain_delta import Model, BrainDelta
 
 import utils
 
-def DeltaCV(X,Y,J,CONF=None):
+def DeltaCV(X, Y, J, Ncv=10):
+    """
+    Perform cross-validation
+
+    The data is divided into Ncv parts which are iterated over. In each iteration
+    the selected chunk is removed and the model trained on the remainder of the
+    data. The trained model is then used to predict age on the left out chunk.
+
+    :param X: Features matrix
+    :param Y: Ages matrix
+    :param J: Number of feature components to use from PCA reduction (0=no PCA reduction)
+    :param Ncv: Number of cross-validation folds
+
+    :return: Tuple of predicted deltas for each subject from model trained on 
+             remainder of data when the chunk containing that subject was left out.
+             In order: (simple model, corrected model, corrected quadratic model, 
+             alternate model, alternate quadratic model)
+    """
     # for slightly improved results, run this many times and average the outputs
     delta1 = np.zeros_like(Y)
     delta2 = np.zeros_like(Y)
@@ -18,65 +34,69 @@ def DeltaCV(X,Y,J,CONF=None):
     delta2q = np.zeros_like(Y)
     delta3q = np.zeros_like(Y)
 
-    Ncv=10 # number of cross-validation folds
-    I = np.random.randint(Ncv, size=len(Y))  # create random cross-validation folds
-
-    if J == 0:
-        J = X.shape[1]
+    # Assign each subject to a random one of Ncv groups
+    SUBJECT_GROUPS = np.random.randint(Ncv, size=len(Y))
 
     b = BrainDelta()
     for i in range(Ncv):
-        OUT, IN = (I==i), np.not_equal(I, i)
-        b.train(Y[IN], X[IN,:], ev_num=J)
-        
-        x_out, y_out = X[OUT,:], Y[OUT]
-        delta1[OUT] = b.predict(y_out, x_out, model=Model.SIMPLE, return_delta=True)
-        delta2[OUT] = b.predict(y_out, x_out, model=Model.UNBIASED, return_delta=True)
-        delta2q[OUT] = b.predict(y_out, x_out, model=Model.UNBIASED_QUADRATIC, return_delta=True)
-        delta3[OUT] = b.predict(y_out, x_out, model=Model.ALTERNATE, return_delta=True)
-        delta3q[OUT] = b.predict(y_out, x_out, model=Model.ALTERNATE_QUADRATIC, return_delta=True)
+        PREDICTION_SET, TRAINING_SET = (SUBJECT_GROUPS==i), (SUBJECT_GROUPS!=i)
+
+        # Train the model on subjects in the training set
+        b.train(Y[TRAINING_SET], X[TRAINING_SET,:], ev_num=J)
+
+        # Get predictions on the subjects not in the training set
+        x_out, y_out = X[PREDICTION_SET,:], Y[PREDICTION_SET]
+        delta1[PREDICTION_SET] = b.predict(y_out, x_out, model=Model.SIMPLE, return_delta=True)
+        delta2[PREDICTION_SET] = b.predict(y_out, x_out, model=Model.UNBIASED, return_delta=True)
+        delta2q[PREDICTION_SET] = b.predict(y_out, x_out, model=Model.UNBIASED_QUADRATIC, return_delta=True)
+        delta3[PREDICTION_SET] = b.predict(y_out, x_out, model=Model.ALTERNATE, return_delta=True)
+        delta3q[PREDICTION_SET] = b.predict(y_out, x_out, model=Model.ALTERNATE_QUADRATIC, return_delta=True)
 
     return delta1, delta2, delta3, delta2q, delta3q
 
 def corr(X, *Y):
+    """
+    Calculate correlation coefficient between X and each array in the sequence Y
+    """
     return np.corrcoef(np.stack([X] + list(Y), axis=1),rowvar=False)[0, 1:]
 
 def main():
+    """
+    Linear simulations (Table 1)
+    """
+    NUM_SIMULATIONS = 10                                               # 20 in paper
+    NUM_SUBJECTS = 10000                                              # 20000 in paper
+    MEAN_Y, RANGE_Y, NOISE_Y_BASE = 60, 25, 0.5                       # 60, 25, 0.5 in MATLAB code
+    NOISE_DELTA = 2                                                   # 2y in paper
+    NOISE_X, NUM_FEATURES_BASE, NUM_FEATURES_MIXED = 0.5, 100, 300   # SIM 1: 0.5, 100, 3000 in paper
+    PCA_NUM_EVALUES = [v for v in  [0, 1, 10, 50, 100, 1000, 2990] if v < NUM_FEATURES_MIXED]
 
-    # linear simulations (Table 1)
-    Nsimulations=10
-    #Js = [0, 1, 10, 50, 100, 1000, 2990]
-    Js = [0, 1, 10, 20]
-    N=1000 # N=10000 in paper
-    AllResults = np.zeros((len(Js), 11, Nsimulations))
-    for sim in range(Nsimulations):
-        j=range(sim)
-        Y = np.random.rand(N) - 0.5 + 0.02*np.random.normal(size=N)
-        Y = 60 + Y*25
+    AllResults = np.zeros((len(PCA_NUM_EVALUES), 11, NUM_SIMULATIONS))
+    print(f"Running simulations: NUM_SUBJECTS={NUM_SUBJECTS}, NUM_FEATURES_BASE={NUM_FEATURES_BASE}, NUM_FEATURES_MIXED={NUM_FEATURES_MIXED}")
+    for sim in range(NUM_SIMULATIONS):
+        print(f"Simulation: {sim+1} of {NUM_SIMULATIONS}")
 
-        deltaTRUE=2*np.random.normal(size=N)
-        minY, maxY = np.min(Y), np.max(Y)
-        Y0 = (Y-minY) / (maxY-minY)
+        # "a fairly sharply truncated (non-Gaussian) distribution for the age range (total range approximately 45-75y),"
+        Y = MEAN_Y + RANGE_Y *(np.random.rand(NUM_SUBJECTS) - 0.5) + NOISE_Y_BASE*np.random.normal(size=NUM_SUBJECTS)
+        DELTA_TRUE = NOISE_DELTA*np.random.normal(size=NUM_SUBJECTS)
 
-        NOISE, ND = 0.5, 100 # SIM 1
-        #NOISE, ND = 10.0, 1 # SIM 2
-        #NOISE, ND = 0.5, 100
-        #deltaTRUE = deltaTRUE*(1 + 0.5*Y0) # SIM 3 non-additive delta
-
-        Yb = Y + deltaTRUE
-        X0 = np.random.normal(size=(N, ND))
+        Yb = Y + DELTA_TRUE
+        X0 = np.random.normal(size=(NUM_SUBJECTS, NUM_FEATURES_BASE))
         X0[:, 0] = Yb
         X0 = utils.normalize(X0)
-        Xmix = np.random.normal(size=(ND, 300))**5 # 3000 in paper
+        Xmix = np.random.normal(size=(NUM_FEATURES_BASE, NUM_FEATURES_MIXED))**5
         X1 = np.dot(X0, Xmix)
         X2 = utils.normalize(X1)
-        X3 = utils.demean(X2+NOISE*np.random.normal(size=X2.shape))
-        for j_idx, J in enumerate(Js): # permuting pcaU gives same results as J=1 (ie null model ~ bad model)
-            print(J)
-            delta1,delta2,delta3,delta2q,delta3q = DeltaCV(X3,Y,J)
+        X3 = utils.demean(X2+NOISE_X*np.random.normal(size=X2.shape))
+        for j_idx, J in enumerate(PCA_NUM_EVALUES): # permuting pcaU gives same results as J=1 (ie null model ~ bad model)
+            if J == 0:
+                print("Running model without PCA reduction")
+            else:
+                print(f"Running model using top {J} eigenvalues")
+            delta1, delta2, delta3, delta2q, delta3q = DeltaCV(X3,Y,J)
             page1, page2, page3 = Y+delta1, Y+delta2, Y+delta3
             agecorr = corr(Y, delta1, page1, page2, page3)
-            corrs = corr(deltaTRUE, delta1, delta2, delta3)
+            corrs = corr(DELTA_TRUE, delta1, delta2, delta3)
             means = np.mean(np.abs([delta1, delta2, delta3]), axis=1)
             AllResults[j_idx, :, sim] = np.concatenate([np.atleast_1d(J), agecorr, means, corrs])
             
@@ -96,7 +116,8 @@ def main():
             "std_corr_delta1", "std_corr_delta2", "std_corr_delta3",
         ]
     )
-    print(df)
+    print(df[["J", "mean_delta1", "mean_delta2", "mean_delta3", "std_delta3", "mean_corr_delta1", "mean_corr_delta2", "mean_corr_delta3"]])
+    df.to_csv("table1.csv")
 
 if __name__ == "__main__":
     main()
