34caef1bccb01c8ca7da5a8ad48c4155c3934226
Martin Craig <martin.craig@nottingham.ac.uk>
2022-08-02 14:53:53 +0100

    Add table plot

--- examples/utils.py
+++ examples/utils.py
@@ -30,6 +30,14 @@ def do_plot(ax, x, y, axlim, title="", horiz=False):
     ax.set_ylim(-axlim, axlim)
     ax.set_title(title)
 
+def do_table(ax, df):
+    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+    #table.set_fontsize(14)
+    #table.scale(1,4)
+    table.auto_set_font_size(False)
+    table.set_fontsize(12)
+    ax.axis('off')
+
 def demean(x, axis=0):
     return x - np.nanmean(x, axis=axis)
 
